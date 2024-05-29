0834d757cddae98baa4310b52322bf9c11c8dfab
AiloZail <166605260+AiloZail@users.noreply.github.com>
2024-05-30 02:57:05 +0700

    Add files via upload

---
+++ Account1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Account1 {
+    int deposit(int x){
+        return x+10000;
+    } 
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        
+        Account1 A1 = new Account1();
+    }
+}
